e5d0a4304040e128c7e241edace02109497fc488
shafeli <silashafeli@gmail.com>
2025-01-03 21:58:13 -0800

    project setup
    main project renaming ect

---
+++ EitirGame/Source/App.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "Core/Core.h"
+
+int main()
+{
+	Core::PrintHelloWorld();
+	std::cout << "From Eitri Game\n";
+	system("pause");
+	return 0;
+}
---
+++ ValkyrieEditor/Source/App.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "Core/Core.h"
+
+int main()
+{
+	Core::PrintHelloWorld();
+	std::cout << "From Val Editor!\n";
+	system("pause");
+	return 0;
+
+}
